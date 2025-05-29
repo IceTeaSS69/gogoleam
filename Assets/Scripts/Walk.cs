29e9c9159561080a91393e4be651eee5231d7fdf
IceTeaSS69 <dima.kuzin.1010@gmail.com>
2025-05-29 20:59:59 +0300

    Speed is changed

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -13,11 +13,11 @@ public class Walk : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.transform.position = gameObject.transform.position + new Vector3(0.05f,0,0);
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0.01f,0,0);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.position = gameObject.transform.position - new Vector3(0.05f, 0, 0);
+            gameObject.transform.position = gameObject.transform.position - new Vector3(0.01f, 0, 0);
         }
     }
 }
